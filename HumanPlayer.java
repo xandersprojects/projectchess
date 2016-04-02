f0eef8f6696e549c47ece0c7513264c531df20e0
Alexander Yu <projectproject711@gmail.com>
2016-04-02 00:47:39 -0700

    Wrote comment summary of 11 groups involved in updated Regex.

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -16,11 +16,39 @@ public class HumanPlayer extends Player {
 
 	/** User inputted proposed move to make. */
 	Move proposeMove(Board board, String move) {
-		String pattern = "^([RNBQK]?|[a-h]?)([a-h]?|[1-8]?)([x]?)(([a-h]+[1-8])|(0-0)|(0-0-0))([=][RNBQ])?([+]|[#])?$";
+		String pattern = "^(([RNBQK]{1}([a-h]|[1-8])?)|([a-h]))?([x])?(([a-h]+[1-8])|(0-0)|(0-0-0))([=][RNBQ])?([+]|[#])?$";
 		Pattern anyMove = Pattern.compile(pattern);
 		Matcher m = anyMove.matcher(move);
 		Move propose;
 		if (m.find()) {
+
+			/** A SUMMARY OF THE 11 GROUPS FROM REGEX PATTERN:
+			  * 1 - Piece to move along with an optional qualifier, OR a Pawn to move
+			  * 2 - Piece to move along with an optional qualifier
+			  * 3 - The optional qualifier
+			  * 4 - The pawn to move
+			  * 5 - The optional "x" denoting a capture
+			  * 6 - The target square, OR a castling move
+			  * 7 - The target square
+			  * 8 - If castling kingside, 0-0 is stored here
+			  * 9 - If castling queenside, 0-0-0 is stored here
+			  * 10 - Optional Promotion handling information, containing the "=" and the piece to promote to
+			  * 11 - Optional check "+" or checkmate "#"
+			*/
+
+			System.out.println();
+			System.out.println("Group 1: " + m.group(1));
+			System.out.println("Group 2: " + m.group(2));
+			System.out.println("Group 3: " + m.group(3));
+			System.out.println("Group 4: " + m.group(4));
+			System.out.println("Group 5: " + m.group(5));
+			System.out.println("Group 6: " + m.group(6));
+			System.out.println("Group 7: " + m.group(7));
+			System.out.println("Group 8: " + m.group(8));
+			System.out.println("Group 9: " + m.group(9));
+			System.out.println("Group 10: " + m.group(10));
+			System.out.println("Group 11: " + m.group(11));
+
 			// propose = new Move(m.group(1), m.group(2), m.group(4), m.group(5));
 			System.out.println("Found");
 		}
